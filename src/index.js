864735f3a0bd5a877c26b2b750d0bc85bccea379
parveen-rx <parveen.soni@rxlogix.com>
2022-04-18 09:15:05 +0530

    changes in the page header

--- src/index.js
+++ src/index.js
@@ -24,24 +24,6 @@ const JEditor1 = jeditor({mock: mock});
 
 render(
   <div>
-    <a target="_blank" href="https://github.com/YMFE/json-schema-editor-visual">
-      <h1>JSON-Schema-Editor</h1>
-    </a>
-    <p style={{ fontSize: '16px' }}>
-      A json-schema editor of high efficient and easy-to-use, base on React.{' '}
-      <a target="_blank" href="https://github.com/YMFE/json-schema-editor-visual">
-        Github
-      </a>
-    </p>
-    <br />
-    <h3>
-      The tool has been used for open source api management platforms：{' '}
-      <a target="_blank" href="https://github.com/ymfe/yapi">
-        YApi
-      </a>
-    </h3>
-
-    <br />
     <h2>Example:</h2>
     <hr />
 
